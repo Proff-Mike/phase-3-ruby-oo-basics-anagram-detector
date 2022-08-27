9d96789eb83ae69c76512f0b30d2f0d8884c0fb2
Michael Okoe <michael.okoejr@gmail.com>
2022-08-27 09:02:47 +0000

    commit tree 882bb242b41c88c06202e4b379b5fd9fbea2e7fb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+#creating a class 'Anagram'
+
+class Anagram
+  attr_accessor :word, :sentence
+  def initialize(word)
+      @word = word
+  end
+
+  def match(sentence)
+      @sentence = sentence
+      @sentence.filter do |wrd|
+          wrd = wrd.chars
+
+          @word.chars.permutation.to_a.include?(wrd) 
+      end
+  end 
+
+end
